fa8b70a72955ae60f714f622bb1eaf38b1253a83
Eduardo Borges <alemaobrk1@gmail.com>
2022-05-24 00:32:35 -0300

    altera navigation

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -10,7 +10,7 @@ export const Navigation = () => {
     <nav>
       { router.asPath !== "/" && <Link href="/">Home</Link> }
       { router.asPath !== "/register-user" && <Link href="/register-user">Novo Usuário</Link> }
-      { router.asPath !== "/edit-user" && <Link href="/edit-user">Editar Usuário</Link> }     
+      {/* { router.asPath !== "/edit-user" && <Link href="/edit-user">Editar Usuário</Link> }      */}
     </nav>
 
     </NavigationHeader>
