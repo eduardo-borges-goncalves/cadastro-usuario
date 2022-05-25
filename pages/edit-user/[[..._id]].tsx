c19767138998e834a028fa59efdbf03551c63af9
Eduardo Borges <alemaobrk1@gmail.com>
2022-05-25 20:11:40 -0300

    insere redirecionamento e resposta de sucesso na edição para o usuário

--- pages/edit-user/[[..._id]].tsx
+++ pages/edit-user/[[..._id]].tsx
@@ -1,70 +1,65 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { FormEvent, useEffect, useState } from "react";
-import { GrayButton } from "../../components/button/GrayButton";
-import { PrimaryBlueButton } from "../../components/button/PrimaryBlueButton";
 import { FormUser } from "../../components/formUser";
 import apiClient from "../../services/api-client";
 import { User } from "../../types/user";
 import { RegisterContainer } from "./style";
 
-export default function RegisterUser () {
-  const [ newUser, setNewUser ] = useState(false)
-  const [ deleted, setDeleted ] = useState(false)
-  const [ user, setUser ] = useState <User>({
-    name: "", 
+export default function RegisterUser() {
+  const [updateUser, setUpdateUser] = useState({
+    new: false,
+    deleted: false,
+    updated: false,
+  })
+  const [user, setUser] = useState<User>({
+    name: "",
     age: "",
     language: "",
-    operationArea: "", 
-    professionalSituation: "", 
-    experience: false, 
-    linkedin: '', 
+    operationArea: "",
+    professionalSituation: "",
+    experience: false,
+    linkedin: '',
     github: ""
   })
 
   const router = useRouter()
-  const {_id} = router.query 
+  const { _id } = router.query
 
   const getUser = async () => {
     const response = await apiClient.get(`/users/${_id}`)
-
     response && setUser(response.data)
   }
 
   const createUser = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    const response = await apiClient.post("/users", {...user})
-    response && setNewUser(true)
-  } 
+    const response = await apiClient.post("/users", { ...user })
+    console.log(response)
+    response && setUpdateUser({ ...updateUser, new: true })
+    setTimeout(()=> router.push(`/view-user/${response.data.user._id}`), 500)
+  }
 
-  useEffect(()=>{ _id && getUser()}, [_id])
+  useEffect(() => { _id && getUser() }, [_id])
 
   return (
     <RegisterContainer>
-      {
-        _id ?
+      { _id ?
           <h1>Editar Usuário</h1>
           :
           <h1>Cadastrar Usuário</h1>
       }
-      <FormUser 
+      <div className={ updateUser.new || updateUser.updated || updateUser.deleted ? "success" : "hidden" }>
+        { updateUser.new && <span >Novo usuário cadastrado com sucesso!</span>}
+        { updateUser.updated && <span >Usuário atualizado com sucesso!</span>}
+        { updateUser.deleted && <span> Usuário deletado!</span>}
+      </div>
+      <FormUser
         createUser={createUser}
         setUser={setUser}
         user={user}
         _id={_id}
-        setDeleted={setDeleted}
+        updateUser={updateUser}
+        setUpdateUser={setUpdateUser}
       />
-      
-      { newUser &&
-        <div className="success">
-          <span >Novo usuário cadastrado com sucesso!</span>  
-        </div>
-      }
-      { deleted &&
-        <div className="success">
-          <span> Usuário deletado!</span>  
-        </div>
-      }
     </RegisterContainer>
   )
 }
--- pages/edit-user/style.ts
+++ pages/edit-user/style.ts
@@ -8,9 +8,19 @@ export const RegisterContainer = styled(HomeContainer)`
   align-items: center;
   flex-direction: column;
 
-  h1 {
-    margin-bottom: 10vh;
+  .success {
+    color: green;
+    font-weight: 600;
+    padding: 1rem;
+    
+    border-radius: 0.25rem;
+    opacity: 0.9;
+    background-color: #d8eed3dc;
+    height: 3rem;
   }
-
-
+  
+  .hidden {
+    height: 3rem;
+    visibility: hidden;
+  }
 `
